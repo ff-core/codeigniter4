2414541eea4f3042cd38e31baca5d3010bcf5d2b
matheus <matheusnarciso@hotmail.com>
2020-12-21 17:24:39 -0300

    database exemples

---
+++ appFFCore/db-exemples.sql
@@ -0,0 +1,110 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.14-MariaDB : Database - exemples
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`exemples` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `exemples`;
+
+/*Table structure for table `atores` */
+
+DROP TABLE IF EXISTS `atores`;
+
+CREATE TABLE `atores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `atores` */
+
+/*Table structure for table `empregados` */
+
+DROP TABLE IF EXISTS `empregados`;
+
+CREATE TABLE `empregados` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_escritorio` int(11) NOT NULL,
+  `nome` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `salario` decimal(7,2) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_EMPREGADOS_ESCRITORIOS` (`id_escritorio`),
+  CONSTRAINT `FK_EMPREGADOS_ESCRITORIOS` FOREIGN KEY (`id_escritorio`) REFERENCES `escritorios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `empregados` */
+
+/*Table structure for table `escritorios` */
+
+DROP TABLE IF EXISTS `escritorios`;
+
+CREATE TABLE `escritorios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cidade` varchar(150) NOT NULL,
+  `telefone` varchar(50) NOT NULL,
+  `endereco` varchar(200) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `escritorios` */
+
+/*Table structure for table `filme` */
+
+DROP TABLE IF EXISTS `filme`;
+
+CREATE TABLE `filme` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) NOT NULL,
+  `ano` varchar(4) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `filme` */
+
+/*Table structure for table `filme_atores` */
+
+DROP TABLE IF EXISTS `filme_atores`;
+
+CREATE TABLE `filme_atores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_filme` int(11) NOT NULL,
+  `id_atores` int(11) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_FILME` (`id_filme`),
+  KEY `FK_ATORES` (`id_atores`),
+  CONSTRAINT `FK_ATORES` FOREIGN KEY (`id_atores`) REFERENCES `atores` (`id`),
+  CONSTRAINT `FK_FILME` FOREIGN KEY (`id_filme`) REFERENCES `filme` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `filme_atores` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
